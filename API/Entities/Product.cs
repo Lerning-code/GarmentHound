d64308de4dbe2bc7e38890c91426a02441ffd870
bhushantalan <bhushan.talan@gmail.com>
2021-08-21 19:42:50 +0530

    change in folder structure

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -1,10 +1,9 @@
-namespace garmenthound.Entities
+namespace API.Entities
 {
     public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-
     }
 }
